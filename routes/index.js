77022419bf9a315cd246df29dd3adde44bb9a5e6
HKprogram <ricknogueira1231@gmail.com>
2022-08-01 15:55:00 -0300

    MY06 - EJS Include

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
+var conn = require('../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+  conn.query("SELECT * FROM tb_menus ORDER BY title", (err, results) => {
+
+    if (err) {
+      console.log(err);
+    }
+
+    res.render('index', { title: 'Restaurante Saboroso!', menus: results },);
+
+  })
+
+});
+
+router.get("/contacts", function (req, res, next) {
+  res.render('contacts', { title: 'Contato - Restaurante Saboroso!', },);
+});
+
+router.get("/menus", function (req, res, next) {
+  res.render('menus', { title: 'Menu - Restaurante Saboroso!', },);
+});
+
+router.get("/reservations", function (req, res, next) {
+  res.render('reservations', { title: 'Reserva - Restaurante Saboroso!', },);
+});
+
+router.get("/services", function (req, res, next) {
+  res.render('services', { title: 'Serviços - Restaurante Saboroso!', },);
 });
 
 module.exports = router;
