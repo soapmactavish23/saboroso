976bc27756a7286b0809158bd8a709bd9b76aecf
HKprogram <ricknogueira1231@gmail.com>
2022-08-01 18:11:07 -0300

    MY07 - Organizando o HEADER com Includes

--- routes/index.js
+++ routes/index.js
@@ -18,19 +18,19 @@ router.get('/', function (req, res, next) {
 });
 
 router.get("/contacts", function (req, res, next) {
-  res.render('contacts', { title: 'Contato - Restaurante Saboroso!', },);
+  res.render('contacts', { title: 'Contato - Restaurante Saboroso!', background: "images/img_bg_3.jpg", h1: 'Diga um oi!', },);
 });
 
 router.get("/menus", function (req, res, next) {
-  res.render('menus', { title: 'Menu - Restaurante Saboroso!', },);
+  res.render('menus', { title: 'Menu - Restaurante Saboroso!', background: "images/img_bg_1.jpg", h1: 'Saboreie nosso menu!', },);
 });
 
 router.get("/reservations", function (req, res, next) {
-  res.render('reservations', { title: 'Reserva - Restaurante Saboroso!', },);
+  res.render('reservations', { title: 'Reserva - Restaurante Saboroso!', background: "images/img_bg_2.jpg", h1: 'Reserve uma Mesa!', },);
 });
 
 router.get("/services", function (req, res, next) {
-  res.render('services', { title: 'Serviços - Restaurante Saboroso!', },);
+  res.render('services', { title: 'Serviços - Restaurante Saboroso!', background: "images/img_bg_1.jpg", h1: 'É um prazer poder servir!', },);
 });
 
 module.exports = router;
