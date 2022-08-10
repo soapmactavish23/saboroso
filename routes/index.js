08b18bd77f80445cc80f59103216fdfae12fb6a0
HKprogram <ricknogueira1231@gmail.com>
2022-08-10 16:56:04 -0300

    MY09 - Organizando o Conteúdo com Include

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,14 @@
 var conn = require('../inc/db');
 var express = require('express');
+var menus = require('../inc/menus');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  conn.query("SELECT * FROM tb_menus ORDER BY title", (err, results) => {
-
-    if (err) {
-      console.log(err);
-    }
-
+  menus.getMenus().then(results => {
     res.render('index', { title: 'Restaurante Saboroso!', menus: results },);
-
-  })
+  });
 
 });
 
@@ -22,7 +17,11 @@ router.get("/contacts", function (req, res, next) {
 });
 
 router.get("/menus", function (req, res, next) {
-  res.render('menus', { title: 'Menu - Restaurante Saboroso!', background: "images/img_bg_1.jpg", h1: 'Saboreie nosso menu!', },);
+
+  menus.getMenus().then(results => {
+    res.render('menus', { title: 'Menu - Restaurante Saboroso!', background: "images/img_bg_1.jpg", h1: 'Saboreie nosso menu!', menus: results })
+  });
+
 });
 
 router.get("/reservations", function (req, res, next) {
