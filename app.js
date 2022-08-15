fca484697a16934b48ab0db85a8effb5acdb4632
soapmactavish23 <ricknogueira1231@gmail.com>
2022-08-15 17:34:58 -0300

    MY14 - Express Session com Redis

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
 
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
@@ -14,6 +16,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  store: new RedisStore({
+    host: 'localhost',
+    port: 6379,
+  }),
+  secret: 'p@ssw0rd',
+  resave: true,
+  saveUninitialized: true,
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,12 +36,12 @@ app.use('/', indexRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,10 @@ router.get('/', function (req, res, next) {
 
 router.get('/login', function (req, res, next) {
 
+    if(!req.session.views) req.session.views = 0;
+
+    console.log(req.session.views);
+
     res.render("admin/login", {
 
     });
