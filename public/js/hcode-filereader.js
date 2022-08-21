e34e4c31522701aba068eb5c58254aef60b500ad
soapmactavish23 <ricknogueira1231@gmail.com>
2022-08-21 16:43:59 -0300

    MY25 - Administração do Menu - Alterando dados do Menu

---
+++ public/js/hcode-filereader.js
@@ -0,0 +1,40 @@
+class HcodeFileReader {
+
+    constructor(inputEl, imgEl) {
+
+        this.inputEl = inputEl;
+        this.imgEl = imgEl;
+
+        this.initInputEvent();
+
+    }
+
+    initInputEvent() {
+
+        document.querySelector(this.inputEl).addEventListener("change", e => {
+            this.render(e.target.files[0]).then(result => {
+                document.querySelector(this.imgEl).src = result;
+            });
+        });
+
+    }
+
+    render(file) {
+        return new Promise((resolve, reject) => {
+
+            let reader = new FileReader();
+
+            reader.onload = function() {
+                resolve(reader.result);
+            }
+
+            reader.onerror = function() {
+                reject("Não foi possível ler a imagem.");
+            }
+
+            reader.readAsDataURL(file);
+
+        })
+    }
+
+}
