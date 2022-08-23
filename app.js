d73e730a0e600d8cc200cd754bb3565e08781d83
soapmactavish23 <ricknogueira1231@gmail.com>
2022-08-23 15:01:41 -0300

    MY28 - Administração do Menu - Alterando dados via POST

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(function(req, res, next) {
     });
   
     form.parse(req, function(err, fields, files) {
+
+      req.body = fields;
       req.fields = fields;
       req.files = files;
   
@@ -50,7 +52,7 @@ app.use(session({
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- inc/menus.js
+++ inc/menus.js
@@ -23,13 +23,37 @@ module.exports = {
 
             fields.photo = `images/${path.parse(files.photo.filepath).base}`
 
-            conn.query(`INSERT INTO tb_menus (title, description, price, photo) VALUES(?,?,?,?)`, [
+            let query, queryPhoto = '', params = [
                 fields.title,
                 fields.description,
-                fields.price,
-                fields.photo
-            ], (err, results) => {
-                if(err) {
+                fields.price
+            ];
+
+            if(files.photo.originalName) {
+                
+                queryPhoto = ", photo = ?";
+                params.push(files.photo)
+            }
+
+
+            if (parseInt(fields.id) > 0) {
+
+                params.push(fields.id);
+
+                query = `UPDATE tb_menus SET title = ?, description = ?, price = ? ${queryPhoto} WHERE id = ?`;
+                
+            } else {
+
+                if(!files.photo) {
+                    reject('Envie a foto do prato.');
+                }
+
+                query = `INSERT INTO tb_menus (title, description, price, photo) VALUES(?,?,?,?)`;
+                
+            }
+
+            conn.query(query, params, (err, results) => {
+                if (err) {
                     reject(err);
                 } else {
                     resolve(results);
