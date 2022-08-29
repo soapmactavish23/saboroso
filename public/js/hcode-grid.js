3736faf598e9a91aa71dd6c07ac0dba85f46fbde
soapmactavish23 <ricknogueira1231@gmail.com>
2022-08-29 15:29:39 -0300

    MY36 - Trabalhando com eventos do componente

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -1,7 +1,14 @@
 class HcodeGrid {
 
     constructor(configs) {
-        this.options = Object.assign({
+
+        configs.listeners = Object.assign({
+            afterUpdateClick: (e) => {
+                $('#modal-update').modal('show');
+            }
+        }, configs.listeners);
+
+        this.options = Object.assign({}, {
             formCreate: "#modal-create form",
             formUpdate: "#modal-update form",
             btnUpdate: ".btn-update",
@@ -30,10 +37,20 @@ class HcodeGrid {
         });
     }
 
+    fireEvent(name, args) {
+
+        if (typeof this.options.listeners[name] === "function") {
+            this.options.listeners[name].apply(this, args);
+        }
+
+    }
+
     initButtons() {
         [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
 
             btn.addEventListener('click', (e) => {
+
+                this.fireEvent('beforeUpdateClick', [e]);
 
                 let tr = e.path.find(el => {
                     return (el.tagName.toUpperCase() === "TR");
@@ -54,7 +71,7 @@ class HcodeGrid {
                     }
                 }
 
-                $('#modal-update').modal('show');
+                this.fireEvent('afterUpdateClick', [e]);
 
             });
 
