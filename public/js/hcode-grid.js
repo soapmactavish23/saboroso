f985d402936970f3b6d59f6d1436c321667c735b
soapmactavish23 <ricknogueira1231@gmail.com>
2022-08-31 15:34:35 -0300

    MY40 - Administração dos usuários criando telas

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -32,6 +32,12 @@ class HcodeGrid {
             formUpdate: "#modal-update form",
             btnUpdate: ".btn-update",
             btnDelete: ".btn-delete",
+            onUpdateLoad: (form, name, data) => {
+
+                let input = form.querySelector('[name='+name+']');
+                if(input) input.value = data[name];
+
+            },
         }, configs);
 
         this.initForms();
