b72baa8d9ccf8a7cbb0ccc39d829d862364eb497
soapmactavish23 <ricknogueira1231@gmail.com>
2022-08-31 15:23:59 -0300

    MY39 - Administração dos usuários criando rotas

--- inc/users.js
+++ inc/users.js
@@ -14,15 +14,15 @@ module.exports = {
                 [
                     email
                 ], (err, results) => {
-                    if(err) {
+                    if (err) {
                         reject(err);
                     } else {
-                        if(!results.length > 0) {
+                        if (!results.length > 0) {
                             reject("Usuário ou senha incorretos");
                         } else {
                             let row = results[0];
 
-                            if(row.password !== password) {
+                            if (row.password !== password) {
                                 reject("Usuário ou senha incorretos");
                             } else {
                                 resolve(row);
@@ -32,5 +32,71 @@ module.exports = {
                 }
             )
         });
+    },
+    getUsers() {
+        return new Promise((resolve, reject) => {
+
+            conn.query("SELECT * FROM tb_users ORDER BY name", (err, results) => {
+
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(results);
+
+            })
+
+        });
+    },
+
+    save(fields, files) {
+        return new Promise((resolve, reject) => {
+
+            let query, queryPhoto = '', params = [
+                fields.name,
+                fields.email,
+            ];
+
+            if (parseInt(fields.id) > 0) {
+
+                params.push(fields.id);
+
+                query = `UPDATE tb_users SET name = ?, email = ? WHERE id = ?`;
+
+            } else {
+
+                query = `INSERT INTO tb_users (name, email, password) VALUES(?, ?, ?)`;
+
+                params.push(fields.password);
+
+            }
+
+            conn.query(query, params, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+
+        });
+    },
+
+    delete(id) {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(`DELETE FROM tb_users WHERE id = ?`, [
+                id
+            ], (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+
+        });
+
     }
 }
