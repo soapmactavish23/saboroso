4c295b157e935880dee69f8d4994a62ebd5ec218
soapmactavish23 <ricknogueira1231@gmail.com>
2022-09-06 14:53:32 -0300

    MY46 - Trabalhando com paginação de dados, botões anterior e próxima

--- inc/Pagination.js
+++ inc/Pagination.js
@@ -66,12 +66,26 @@ class Pagination {
             nrend = this.getCurrentPage() + parseInt(limitPageNav / 2);
         }
 
+        if(this.getCurrentPage() > 1) {
+            links.push({
+                text: '<<',
+                href: '?' + this.getQueryString(Object.assign({}, params, {page: this.getCurrentPage() - 1}))
+            });
+        }
+
         for(let x = nrstart; x <= nrend; x++) {
             links.push({
                 text: x,
                 href: `?${this.getQueryString(Object.assign({}, params, {page: x}))}`,
                 active: (x === this.getCurrentPage())
             })
+        }
+
+        if(this.getCurrentPage() < this.getTotalPages()) {
+            links.push({
+                text: '>>',
+                href: '?' + this.getQueryString(Object.assign({}, params, {page: this.getCurrentPage() + 1}))
+            });
         }
 
         return links;
